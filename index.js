c212ff1d643b79eca3327dd41364ddf2119cb234
Han Yoong <b_wars@yahoo.com>
2018-09-11 01:55:25 +0800

    Modify to point to App instead of storyboard.

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ import {name as appName} from './app.json';
 /**
  * Factor manual code trigger to change between storybook view or real app view.
  **/
-AppRegistry.registerComponent(appName, () => Storybook);
-//AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => Storybook);
+AppRegistry.registerComponent(appName, () => App);
